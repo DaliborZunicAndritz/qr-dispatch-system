5025a058202601671de1630c8b7ca47fb383939b
Dalibor Zunic <Dalibor.Zunic@andritz.com>
2022-11-29 12:46:39 +0100

    Add data fetch service

--- src/app/components/main-data/main-data.component.ts
+++ src/app/components/main-data/main-data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataDetails } from 'src/app/models/data-details';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-main-data',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDataComponent implements OnInit {
 
-  constructor() { }
+  dataDetails = {} as DataDetails
+
+  constructor(private route: ActivatedRoute, private dataService: DataService) { }
 
   ngOnInit(): void {
-  }
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get("id");
 
+        if (id) {
+          this.dataService.getDataByGuid(id).subscribe({
+            next: (response) => {
+              this.dataDetails = response;
+            }
+          });
+        }
+      }
+    });
+
+  }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { DataDetails } from '../models/data-details';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  baseApiUrl: string = environment.baseApiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getDataByGuid(id: string): Observable<DataDetails> {
+    return this.http.get<DataDetails>(`${this.baseApiUrl}/api/DataDetails/${id}`)
+  }
+}
